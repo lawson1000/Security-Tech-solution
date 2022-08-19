d10c92bbab3863543d128169cfa02d95c146ab01
lawson1000 <lawsonolanrewaju@yahoo.com>
2022-08-19 01:33:04 +0100

    third commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $("#nav-icon").click(function(){
+        $("ul").toggleClass("show");
+        $("body").toggleClass("hide-overflow");
+    })
+    const toTop = document.querySelector(".to-top")
+    window.addEventListener("scroll", () =>{
+    if(window.pageYOffset > 100){
+        toTop.classList.add("press")
+    } else{
+        toTop.classList.remove("press")
+    }
+})
+});
+
