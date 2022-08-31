19b7a80d9199e019dbaf71e53a9e245d4f6c83e8
lawson1000 <lawsonolanrewaju@yahoo.com>
2022-08-31 11:11:22 +0100

    committed

--- js/main.js
+++ js/main.js
@@ -19,11 +19,30 @@ $(document).ready(function(){
             }
         });
     
-
+    $(".carousel").owlCarousel({
+        margin:100,
+        loop:true,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        autoplayHoverPause: true,
+        responsive:{
+            0:{
+                items:2,
+                nav:false
+            },
+            600:{
+                items:3,
+                nav: false
+            },
+            1000:{
+                items:4,
+                nav:false
+            }
+        }
+    })
     
 });
 
 function myFunction(){
-   document.getElementById("btn-read").onclick = location.href = "contact.html";
+   document.getElementById("btn-read").onclick = location.href = "OurServices.html";
 }
-
